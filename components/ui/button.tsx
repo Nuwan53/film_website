db36f1731a26874b8b694b09197c536a09229b42
Nuwan <nuwanrathnayaka53@gmail.com>
2025-07-24 00:17:18 +0530

    add release dates for films and implement SignInButton component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { useRouter } from "next/router"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
@@ -39,8 +40,24 @@ export interface ButtonProps
   asChild?: boolean
 }
 
-
+//connect signin page to this button
+export function SignInButton(props: ButtonProps) {
+  const router = useRouter()
+  return (
+    <Button
+      {...props}
+      onClick={(e) => {
+        props.onClick?.(e)
+        if (!e.defaultPrevented) {
+          router.push("/signin.")
+        }
+      }}
+    />
+  )
+}
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+    
+
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
